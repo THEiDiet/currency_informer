caa5dd225f56961d14466098175bfae276752805
THEiDiet <76583093+THEiDiet@users.noreply.github.com>
2022-03-21 16:07:39 +0700

    fix bug in setInterval

--- src/utils/getLastTenCurrensyList.js
+++ src/utils/getLastTenCurrensyList.js
@@ -1,26 +1,47 @@
 import { SessionStorage, Times } from '../constants'
 
-export const getLastTenCurrencyList = (link, handleCurrencyList) => {
+// export const getLastTenCurrencyList = (link, handleCurrencyList) => {
+//   let counter = 0
+//   const arrayOfLatestCurrencyLists = []
+//   let currentLink = link
+//   const fetchData = async () => {
+//     const response = await fetch(currentLink)
+//     const data = await response.json()
+//     currentLink = data.PreviousURL
+//     arrayOfLatestCurrencyLists.push(data)
+//     // eslint-disable-next-line no-plusplus
+//     counter++
+//     if (counter >= 10) {
+//       clearInterval(timerId)
+//       sessionStorage.setItem(
+//         SessionStorage.CurrencyKey,
+//         JSON.stringify(arrayOfLatestCurrencyLists),
+//       )
+//       handleCurrencyList(arrayOfLatestCurrencyLists)
+//     }
+//   }
+//   const timerId = setInterval(fetchData, Times.FetchLatestCurrencyListsInterval)
+// }
+export const getLastTenCurrencyList = (link, handleList) => {
   let counter = 0
   const arrayOfLatestCurrencyLists = []
   let currentLink = link
   const fetchData = async () => {
     const response = await fetch(currentLink)
-    // eslint-disable-next-line no-debugger
-    // debugger
     const data = await response.json()
     currentLink = data.PreviousURL
     arrayOfLatestCurrencyLists.push(data)
     // eslint-disable-next-line no-plusplus
     counter++
+    if (counter < 10) setTimeout(fetchData, Times.FetchLatestCurrencyListsInterval)
     if (counter >= 10) {
-      clearInterval(timerId)
+      clearTimeout(timeId)
       sessionStorage.setItem(
         SessionStorage.CurrencyKey,
         JSON.stringify(arrayOfLatestCurrencyLists),
       )
-      handleCurrencyList(arrayOfLatestCurrencyLists)
+      handleList(arrayOfLatestCurrencyLists)
     }
   }
-  const timerId = setInterval(fetchData, Times.FetchLatestCurrencyListsInterval)
+  const timeId = setTimeout(fetchData, Times.FetchLatestCurrencyListsInterval)
 }
